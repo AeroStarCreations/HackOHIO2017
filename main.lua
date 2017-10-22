e2a45192b299c97e078969aa0f17f0e08e7f39b7
Benjamin Higgins <higgins.400@osu.edu>
2017-10-21 23:16:46 -0400

    Ben updated calculations

--- main.lua
+++ main.lua
@@ -10,9 +10,9 @@ local function findDistance(lat1, lon1, alt1, lat2, lon2, alt2)
     lat2ft = lat2*(69*5280)
     lon1ft = lon1*(69*5280)
     lon2ft = lon2*(69*5280)
-    alt1ft = alt1*(69*5280)
-    alt2ft = alt2*(69*5280)
-    distance = ((lat1ft- lat2ft)^2 + (lon1ft - lon2ft)^2 + (alt1ft - alt2ft)^2)^(1/2)
+    alt1ft = alt1*(3.28084)
+    alt2ft = alt2
+    distance = (((lat1ft- lat2ft)^2) + ((lon1ft - lon2ft)^2) + ((alt1ft - alt2ft)^2))^(1/2)
     return distance
 end
 
@@ -21,25 +21,23 @@ local locationHandler = function( event )
     local latitudeText1 = string.format( "Latitude:\n %.4f\n", event.latitude )
     local longitudeText1 = string.format( "Longitude:\n%.4f", event.longitude )
     local altitudeText1 = string.format( "Altititude:\n%.4f", event.altitude)
-    local latitude1 = display.newText( latitudeText1, 150, 150, native.systemFont, 16 )
-    local longitude1 = display.newText( longitudeText1, 150, 225, native.systemFont, 16 )
-    local altitude1 = display.newText( altitudeText1, 150, 300, native.systemFont, 16)
+    local latitude1 = display.newText( latitudeText1, display.contentWidth/2, display.contentHeight/4, native.systemFont, 45 )
+    local longitude1 = display.newText( longitudeText1, latitude1.x, display.contentHeight/2, native.systemFont, 45 )
+    local altitude1 = display.newText( altitudeText1, latitude1.x, display.contentHeight*(3/4), native.systemFont, 45)
 
     --given coordinate values for user 2
-    lat2 = 34.00*(69*5280)
-    lon2 = -134.00*(69*5280)
-    alt2 = 3.00*(69*5280)
+    lat2 = 39.9980
+    lon2 = -83.0142
+    alt2 = 738
     --call function findDistance to find distance in feet
     distance = findDistance(event.latitude, event.longitude, event.altitude, lat2, lon2, alt2)
     local distanceText = string.format( "Distance:\n %.4f feet", distance)
-    local distance1 = display.newText(distanceText, 150, 375, native.systemFont, 16)
+    local distance1 = display.newText(distanceText, latitude1.x, display.contentHeight*.9, native.systemFont, 45)
 end
 
 Runtime:addEventListener( "location", locationHandler )
 
-<<<<<<< HEAD
-=======
-local GS = require("plugin.gamesparks")
+--[[ local GS = require("plugin.gamesparks")
 
 local function writeText(string)
     print(string)
@@ -105,14 +103,13 @@ getLocationRequest:send(function(response)
     else local POS = response:getScriptData().POS
         for key,value in pairs(POS) do print(key,value) end
     end
-end)
+end) ]]
 
-local cameraImage = display.newRect(display.contentCenterX, display.contentCenterY, display.contentHeight*(3/4), display.contentHeight)
+--[[ local cameraImage = display.newRect(display.contentCenterX, display.contentCenterY, display.contentHeight*(3/4), display.contentHeight)
 cameraImage.fill = { type = "camera" }
 
 local function updateCamera()
     cameraImage.fill = { type="camera" }
 end
 
- Runtime:addEventListener("enterFrame", updateCamera)
->>>>>>> 7baba23a92b882f8cd8c1d5984bbe5927330965f
+ Runtime:addEventListener("enterFrame", updateCamera) ]]
