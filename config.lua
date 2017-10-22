7baba23a92b882f8cd8c1d5984bbe5927330965f
Nathan Balli <nathanballi@Nathans-MacBook-Pro.local>
2017-10-21 22:23:35 -0400

    updated with camera

--- config.lua
+++ config.lua
@@ -3,21 +3,16 @@
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
 
-application =
-{
-	content =
-	{
-		width = 320,
-		height = 480, 
-		scale = "letterbox",
-		fps = 60,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
-	},
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
+application = {
+    content = {
+        width = aspectRatio > 1.5 and 800 or math.floor( 1200 / aspectRatio ),
+        height = aspectRatio < 1.5 and 1200 or math.floor( 800 * aspectRatio ),
+        scale = "letterBox",
+        fps = 60,
+        imageSuffix = {
+             ["@2x"] = 1.3,
+        },
+    },
 }
--- main.lua
+++ main.lua
@@ -4,5 +4,79 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
-print "Hello World"
+local GS = require("plugin.gamesparks")
+
+local function writeText(string)
+    print(string)
+end
+
+local function availabilityCallback(isAvailable)
+    writeText("Availability: " .. tostring(isAvailable) .. "\n")
+
+    if isAvailable then
+        --Do something
+    end
+
+end
+
+--Create GS Instance
+gs = createGS()
+--Set the logger for debugging the Responses, Messages and Requests flowing in and out
+gs.setLogger(writeText)
+--Set API Key
+gs.setApiKey("z321094vxS3F")
+--Set Secret
+gs.setApiSecret("gfAMnRRDZaueUFWF3VgMDNADBP1y0nlu")
+--Set Credential
+gs.setApiCredential("device")
+--Set availability callback function
+gs.setAvailabilityCallback(availabilityCallback)
+--Connect to your game's backend
+gs.connect()
+
+--------------------Authentication
+--Build request
+local requestBuilder = gs.getRequestBuilder()
+local deviceAuthenticationRequest = requestBuilder.createDeviceAuthenticationRequest()
+local saveLocationRequest = requestBuilder.createLogEventRequest()
+local getLocationRequest = requestBuilder.createLogEventRequest()
+
+--Set values
+deviceAuthenticationRequest:setDeviceId(system.getInfo("deviceID"))
+deviceAuthenticationRequest:setDeviceOS(system.getInfo("platform"))
+saveLocationRequest:setEventKey("Set_Position")
+--Emily's function.
+saveLocationRequest:setEventAttribute("POS",{X=23,Y=23,Z=23})
+getLocationRequest:setEventKey("Get_Position")
+
+--Send and print authentication token
+deviceAuthenticationRequest:send(function(authenticationResponse)
+    writeText("token: "..authenticationResponse:getAuthToken().."\n")
+end)
+
+--Send request and check for errors
+saveLocationRequest:send(function(response)
+    if response:hasErrors() then
+        for key,value in pairs(response:getErrors()) do print(key,value) end
+    end
+end)
+
+--Send request.
+getLocationRequest:send(function(response)
+    --Process Response
+    if response:hasErrors() then--If errors then print errors
+        for key,value in pairs(response:getErrors()) do print(key,value) end
+        --Else extract the POS variable (Type table) and print the contents within it
+    else local POS = response:getScriptData().POS
+        for key,value in pairs(POS) do print(key,value) end
+    end
+end)
+
+local cameraImage = display.newRect(display.contentCenterX, display.contentCenterY, display.contentHeight*(3/4), display.contentHeight)
+cameraImage.fill = { type = "camera" }
+
+local function updateCamera()
+    cameraImage.fill = { type="camera" }
+end
+
+ Runtime:addEventListener("enterFrame", updateCamera)
