34a26384b5335045f2f60920724889faa7caec2d
Benjamin Higgins <higgins.400@osu.edu>
2017-10-21 22:25:18 -0400

    I quit API gamespark

--- main.lua
+++ main.lua
@@ -5,7 +5,6 @@
 -----------------------------------------------------------------------------------------
 
 local GS = require("plugin.gamesparks")
-
 local function writeText(string)
     print(string)
 end
@@ -33,6 +32,7 @@ gs.setApiCredential("device")
 gs.setAvailabilityCallback(availabilityCallback)
 --Connect to your game's backend
 gs.connect()
+multiplayer = gs.getMultiplayer()
 
 --------------------Authentication
 --Build request
@@ -41,36 +41,49 @@ local deviceAuthenticationRequest = requestBuilder.createDeviceAuthenticationReq
 local saveLocationRequest = requestBuilder.createLogEventRequest()
 local getLocationRequest = requestBuilder.createLogEventRequest()
 
---Set values
-deviceAuthenticationRequest:setDeviceId(system.getInfo("deviceID"))
-deviceAuthenticationRequest:setDeviceOS(system.getInfo("platform"))
-saveLocationRequest:setEventKey("Set_Position")
---Emily's function.
-saveLocationRequest:setEventAttribute("POS",{X=23,Y=23,Z=23})
-getLocationRequest:setEventKey("Get_Position")
+authenticateTheUser = function()
+    --Set values
+    deviceAuthenticationRequest:setDeviceId(system.getInfo("deviceID"))
+    deviceAuthenticationRequest:setDeviceOS(system.getInfo("platform"))
 
---Send and print authentication token
-deviceAuthenticationRequest:send(function(authenticationResponse)
-    writeText("token: "..authenticationResponse:getAuthToken().."\n")
-end)
+    --Send and print authentication token
+    deviceAuthenticationRequest:send(function(authenticationResponse)
+        writeText("token: "..authenticationResponse:getAuthToken().."\n")
+    end)
 
---Send request and check for errors
-saveLocationRequest:send(function(response)
-    if response:hasErrors() then
-        for key,value in pairs(response:getErrors()) do print(key,value) end
+    -- Call Emily's function to set long., lat., and alt.
+    sendLocation = function(Longitude, Latitude, Altitude)
+        saveLocationRequest:setEventKey("Set_Position")
+        saveLocationRequest:setEventAttribute("POS",{X=Longitude,Y=Latitude,Z=Altitude})
     end
-end)
+end
 
 --Send request.
-getLocationRequest:send(function(response)
-    --Process Response
-    if response:hasErrors() then--If errors then print errors
-        for key,value in pairs(response:getErrors()) do print(key,value) end
-        --Else extract the POS variable (Type table) and print the contents within it
-    else local POS = response:getScriptData().POS
-        for key,value in pairs(POS) do print(key,value) end
-    end
-end)
+getLocation = function()
+    getLocationRequest:setEventKey("Get_Position")
+    saveLocationRequest:send(function(response)
+        if response:hasErrors() then
+            for key,value in pairs(response:getErrors()) do print(key,value) end
+        end
+    end)
+    getLocationRequest:send(function(response)
+        --Process Response
+        if response:hasErrors() then--If errors then print errors
+            for key,value in pairs(response:getErrors()) do print(key,value) end
+            --Else extract the POS variable (Type table) and print the contents within it
+        else local POS = response:getScriptData().POS
+            for key,value in pairs(POS) do print(key,value) end
+        end
+    end)
+end
 
-local cameraImage = display.newRect(display.contentCenterX, display.contentCenterY, display.contentHeight, display.contentWidth)
-cameraImage.fill = { type = "camera" }
+authenticateTheUser()
+
+sendLocation(2,4,7)
+getLocation()
+
+sendLocation(1,1,2.3)
+getLocation()
+
+--local cameraImage = display.newRect(display.contentCenterX, display.contentCenterY, display.contentHeight, display.contentWidth)
+--cameraImage.fill = { type = "camera" }
